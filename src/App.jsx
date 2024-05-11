466f03c04eb41c69c3e84337f118b20f6b6f39ee
Narathip <narathip2002@outlook.co.th>
2024-05-11 13:18:35 +0700

    add tailwind injectFirst

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import Button from '@mui/material/Button';
-import { PieChart } from '@mui/x-charts/PieChart';
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+import Button from "@mui/material/Button";
+import { PieChart } from "@mui/x-charts/PieChart";
 
 export default function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      
+
       <AccessAlarmIcon />
-      
-      <Button variant="outlined">Outlined</Button>
-      
+
+      <Button className="border-dotted border-2 border-red-500">
+        Outlined
+      </Button>
+
       <PieChart
-      series={[
-        {
-          data: [
-            { id: 0, value: 10, label: 'series A' },
-            { id: 1, value: 15, label: 'series B' },
-            { id: 2, value: 20, label: 'series C' },
-          ],
-        },
-      ]}
-      width={400}
-      height={200}
-    />
+        series={[
+          {
+            data: [
+              { id: 0, value: 10, label: "series A" },
+              { id: 1, value: 15, label: "series B" },
+              { id: 2, value: 20, label: "series C" },
+            ],
+          },
+        ]}
+        width={400}
+        height={200}
+      />
     </>
   );
 }
