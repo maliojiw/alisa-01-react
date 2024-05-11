06a22d8db52e5f1236e0c4d1eac815cefa868980
Narathip <narathip2002@outlook.co.th>
2024-05-11 16:59:17 +0700

    delete mui / just react tailwind

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Quick start
+## Quick start
 
 - clone the repo: git clone https://github.com/Narathip99/react-js.git
 - install dependencies: `npm install`
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-//
-import { StyledEngineProvider } from "@mui/material";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <App />
-    </StyledEngineProvider>
+    <App />
   </React.StrictMode>
 );
