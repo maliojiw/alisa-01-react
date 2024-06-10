6d24c2653f9de0f0d611ae86665fd9fee5dfc737
Alisa <maliojiw@gmail.com>
2024-06-10 13:02:21 +0700

    Update src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Home from "./components/Home.jsx";
+import Owner from "./components/Owner.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+      
+    ,
+  },
+  {
+    path: "/Owner",
+    element: <Owner/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
