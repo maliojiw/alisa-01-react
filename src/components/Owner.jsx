b8f4ce73d381c8849b70deeee79a20c8362e6c8d
Alisa <maliojiw@gmail.com>
2024-06-10 18:06:24 +0700

    update

--- src/components/Owner.jsx
+++ src/components/Owner.jsx
@@ -11,14 +11,14 @@ export default function Owner() {
         <div className="flex flex-col items-center bg-white shadow-lg rounded-lg p-8 max-w-screen-lg	">
           <div className="w-64 h-64 flex items-center justify-center mb-4 overflow-hidden rounded-full">
             <img 
-              src="https://img2.pic.in.th/pic/download7563b46659eb7374.jpg" 
+              src="https://img2.pic.in.th/pic/56902419_2123143234469096_4356620653885915136_n.jpg" 
               alt="Profile" 
               className="object-cover h-full w-full"
             />
           </div>
           <h3 className="text-xl font-semibold text-gray-700 mb-4">About me</h3>
           <p className="text-center text-gray-600 leading-relaxed">
-            test test
+          I'm Egg, and this project is part of the JSD7 Boostcamp. I spent half a day working on it using React.js.
           </p>
         </div>
       </main>
