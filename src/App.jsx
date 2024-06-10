ed404c6f4cd3863c4d1d7138cbf455fd390852c8
Alisa <maliojiw@gmail.com>
2024-06-10 13:13:29 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-export default function App() {
+import React from 'react'
+import Home from './components/Home'
+function App() {
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
+    <Home users={usersData} />
+
+  )
 }
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+export default function Nav({ onReset }) {
+  const navigate = useNavigate();
+
+  const handleHomeClick = (e) => {
+    e.preventDefault();
+    if (onReset) {
+      onReset(); 
+    }
+    navigate('/'); 
+  };
+
+  return (
+    <div>
+      <header className="bg-gray-300 p-4 flex justify-between items-center">
+        <h1 className="text-xl font-bold">Kai Kai React - Assessment</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <a href="/" onClick={handleHomeClick}>Home</a>
+            </li>
+            <li>
+              <Link to='/Owner'>Owner</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
